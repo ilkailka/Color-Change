69251fa19b3c2f03535902ce2372eea8a42833cb
ilka <ilka.ilkoy@gmail.com>
2023-07-30 18:02:39 +0200

    New function

--- Color Change/ViewController.swift
+++ Color Change/ViewController.swift
@@ -9,22 +9,46 @@ import UIKit
 
 final class ViewController: UIViewController {
     
+    // MARK: - IB Outlets
     @IBOutlet var mainLabel: UIView!
+    
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
+    
     @IBOutlet var redSliderValue: UILabel!
     @IBOutlet var greenSliderValue: UILabel!
     @IBOutlet var blueSliderValue: UILabel!
-
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        mainLabel.layer.cornerRadius = 10
     }
 
+    // MARK: - IB Actions
     @IBAction func redValueChange() {
-        redSliderValue.text = NSString(redSlider.value)
+        redSliderValue.text = String(format: "%.2f", redSlider.value)
+        labelColorChange()
     }
     
-
+    @IBAction func greenValueChange() {
+        greenSliderValue.text = String(format: "%.2f", greenSlider.value)
+        labelColorChange()
+    }
+    
+    @IBAction func blueValueChange() {
+        blueSliderValue.text = String(format: "%.2f", blueSlider.value)
+        labelColorChange()
+    }
+    
+    // MARK: - Private Methods
+    private func labelColorChange() {
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        mainLabel.backgroundColor = color
+    }
 }
 
